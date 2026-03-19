fc2abe72092c05e7cf1b97f2b2774d68706d9ab3
janith jayarisi wijesinghe <100828158+janithwijesinghe@users.noreply.github.com>
2026-03-19 09:26:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -8,8 +9,10 @@ import CreativeProcess from './components/CreativeProcess';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import PortfolioPage from './pages/PortfolioPage';
+import ProjectDetail from './pages/ProjectDetail';
 
-function App() {
+function HomePage() {
   return (
     <div className="min-h-screen bg-[#0a0a0f] text-white">
       <Navbar />
@@ -26,6 +29,16 @@ function App() {
   );
 }
 
-export default App;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+      </Routes>
+    </Router>
+  );
+}
 
- 
+export default App;
